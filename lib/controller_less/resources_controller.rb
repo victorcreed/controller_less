e23ccd9c23ba1a90948c693b6dc8764cf6c0798b
victor creed <a2ninek@yahoo.com>
2016-03-14 12:46:36 +0500

    data cleaned

--- lib/controller_less/resources_controller.rb
+++ lib/controller_less/resources_controller.rb
@@ -5,8 +5,7 @@ module ControllerLess
     end
     def self.cl_config=(config)
       @cl_config = config
-      self.controller_name = config.controller_name
-      self.resource_class = config.resource_class
+      defaults resource_class: config.resource_class
     end
     def cl_config
       self.class.cl_config
@@ -14,31 +13,6 @@ module ControllerLess
     def self.inherited(base)
       super(base)
       base.override_resource_class_methods!
-    end
-
-    def collection
-      get_collection_ivar || begin
-      collection = find_collection
-      set_collection_ivar collection
-      end
-    end
-
-    def find_collection(options = {})
-      collection = scoped_collection
-      collection
-    end
-    def scoped_collection
-      end_of_association_chain
-    end
-
-    def resource
-      get_resource_ivar || begin
-      resource = find_resource
-      set_resource_ivar resource
-      end
-    end
-    def find_resource
-      scoped_collection.send method_for_find, params[:id]
     end
 
     def self.override_resource_class_methods!
@@ -49,13 +23,6 @@ module ControllerLess
           @cl_config ? @cl_config.resource_class : nil
         end
 
-        def self.controller_name=(name); end
-        def self.controller_name
-          @cl_config.try(:controller_name)
-        end
-        def controller_name
-          self.class.controller_name
-        end
         def resource_class
           self.class.resource_class
         end
